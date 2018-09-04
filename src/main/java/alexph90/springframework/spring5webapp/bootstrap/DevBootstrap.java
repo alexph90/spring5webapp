c60e3533454bed01b1d5222afc8a4a49a0cdaadd
Alex <alexph90@gmail.com>
2018-09-04 20:18:57 +0100

    Initial commit.

---
+++ src/main/java/alexph90/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -0,0 +1,55 @@
+package alexph90.springframework.spring5webapp.bootstrap;
+
+
+import alexph90.springframework.spring5webapp.model.Author;
+import alexph90.springframework.spring5webapp.model.Book;
+import alexph90.springframework.spring5webapp.model.Publisher;
+import alexph90.springframework.spring5webapp.repositories.AuthorRepository;
+import alexph90.springframework.spring5webapp.repositories.BookRepository;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by jt on 5/16/17.
+ */
+@Component
+public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+    private AuthorRepository authorRepository;
+    private BookRepository bookRepository;
+
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        initData();
+    }
+
+    private void initData(){
+
+        //Eric
+        Author eric = new Author("Eric", "Evans");
+        Publisher harperCollins = new Publisher("Harper Collins", "Harper Collins Address");
+        Book  ddd = new Book("Domain Driven Design", "1234", harperCollins);
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+
+        //Rod
+        Author rod = new Author("Rod", "Johnson");
+        Publisher wrox = new Publisher("Wrox", "Wrox Address");
+        Book noEJB = new Book("J2EE Development without EJB", "23444", wrox );
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+    }
+}
---
+++ src/main/java/alexph90/springframework/spring5webapp/repositories/PublisherRepository.java
@@ -0,0 +1,7 @@
+package alexph90.springframework.spring5webapp.repositories;
+
+import alexph90.springframework.spring5webapp.model.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long> {
+}
